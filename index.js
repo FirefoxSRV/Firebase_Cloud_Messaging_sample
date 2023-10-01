14765757229fdb4854c8de9c78d6d08afd77e028
FirefoxSRV <shreyasvisweshwaran@gmail.com>
2023-10-01 15:00:17 +0530

    Fixed bigs FCM

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {initializeApp, applicationDefault} from 'firebase-admin';
+import {initializeApp, applicationDefault} from 'firebase-admin/app';
 import {getMessaging} from "firebase-admin/messaging";
 import express, {json} from "express";
 
